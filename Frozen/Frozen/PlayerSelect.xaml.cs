a0a7d428737d7e7fddb25bdc8980e4f676b115f2
False-Victory <57667360+False-Victory@users.noreply.github.com>
2020-10-28 15:28:42 +0100

    Add files via upload
    Frozen Thema

---
+++ Frozen/Frozen/PlayerSelect.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frozen
+{
+    /// <summary>
+    /// Interaction logic for StartScreen.xaml
+    /// </summary>
+    public partial class StartScreen : Window
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
